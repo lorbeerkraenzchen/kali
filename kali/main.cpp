714663bc12de9c540f6eee1852bdfac17ca86d10
Lars <laaaureeel@gmail.com>
2024-09-18 15:58:54 +0200

    bhop

--- kali/main.cpp
+++ kali/main.cpp
@@ -1,9 +1,43 @@
-﻿#include <iostream>
+﻿#include "memory/memory.h"
+#include <iostream>
+#include "offsets.hpp"
+#include <thread>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	const auto mem = Memory("cs2.exe");
+	const auto client = mem.GetModuleAddress("client.dll");
+
+	const auto jump = client + offsets::jump;
+
+	while (true)
+	{
+		const uintptr_t dwLocalPlayerPawn = mem.Read<uintptr_t>(client + offsets::dwLocalPlayerPawn);
+
+		if (!dwLocalPlayerPawn)
+		{
+			continue;
+		}
+
+		const uint32_t m_fFlags = mem.Read<uint32_t>(dwLocalPlayerPawn + offsets::m_fFlags);
+
+		if (GetAsyncKeyState(VK_SPACE))
+		{
+			if (m_fFlags == 65665 || m_fFlags == 65667)
+			{
+				this_thread::sleep_for(chrono::milliseconds(1));
+				mem.Write<uint32_t>(jump, 65537);
+			}
+			else
+			{
+				mem.Write<uint32_t>(jump, 256);
+			}
+		}
+
+		this_thread::sleep_for(chrono::milliseconds(1));
+	}
+
 	return 0;
 }
---
+++ kali/offsets.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace offsets
+{
+	constexpr std::ptrdiff_t dwLocalPlayerPawn = 0x17C17F0;
+	constexpr std::ptrdiff_t jump = 0x17BA530;
+	constexpr std::ptrdiff_t m_fFlags = 0x3CC;
+}
